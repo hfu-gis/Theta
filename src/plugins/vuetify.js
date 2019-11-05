2356affe921279299504cddcc4e6e592b3c040e0
xcg3692 <alfred.walther-weyland.extern@fiduciagad.de>
2019-11-05 23:16:46 +0100

    Fixes #2: Vuetify installiert

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,21 @@
-// src/plugins/vuetify.js
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+Vue.use(Vuetify);
 
-Vue.use(Vuetify)
-
-const opts = {}
-
-export default new Vuetify(opts)
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#ee44aa',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      },
+    },
+  },
+});
