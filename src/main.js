d8fe5117343dd9ae69c1259b7dc289fccd2588eb
leonschafer <leon.schaefer@hs-furtwangen.de>
2019-11-13 12:51:28 +0100

    Merge branch 'master' of https://github.com/hfu-gis/demo
    # Conflicts:
    #	README.md

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import router from './router'
+
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  router: router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Template.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="body-1">
+    <!-- HTML-Code aus dem Body-Tag hier einfügen -->
+  </div>
+</template>
+
+<script>
+export default {
+  // gebt jeder Page einen eigenen Namen
+  name: 'template',
+
+  // benötigte Komponenten
+  components: {},
+
+  // entspricht den HTML-Attributen
+  props: {},
+
+  // Variablen-Speicher
+  data() {
+    return {}
+  },
+
+  // reagieren auf prop-Veränderung
+  watch: {},
+
+  // interne Methoden
+  methods: [],
+
+  // Initialisierung
+  created() {}
+}
+</script>
+
+<style scoped>
+/* CSS für diese Seite hier einfügen */
+</style>
