ef29a7e9ae79f53f3ebb78ec8731fde1d1a40308
erpunkt <rebecca.rachel.raeschke@hs-furtwangen.de>
2019-11-19 00:56:10 +0100

    Merge branch 'master' of https://github.com/hfu-gis/demo
    # Conflicts:
    #	.gitignore
    #	README.md
    #	package-lock.json
    #	package.json
    #	public/index.html
    #	src/App.vue
    #	src/components/HelloWorld.vue
    #	src/main.js

---
+++ src/views/Nutzungsbedingungen.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="body-1">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Nutzungsbedingungen"
+    }
+</script>
+
+<style scoped>
+
+</style>
