50afe2200afcfd1aa35446e1805ab7710eac3164
erpunkt <rebecca.rachel.raeschke@hs-furtwangen.de>
2019-11-20 12:16:19 +0100

    Merge branch 'master' of https://github.com/hfu-gis/demo
    # Conflicts:
    #	.gitignore
    #	README.md
    #	package-lock.json
    #	package.json
    #	public/index.html
    #	src/App.vue
    #	src/components/HelloWorld.vue
    #	src/main.js

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="body-1">
+        <!-- HTML-Code aus dem Body-Tag hier einfügen -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
