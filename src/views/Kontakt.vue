7ddde839173c9fd2484f202bbb1c4476e651c339
erpunkt <rebecca.rachel.raeschke@hs-furtwangen.de>
2019-11-20 15:44:40 +0100

    Changes on Sites

--- src/views/Kontakt.vue
+++ src/views/Kontakt.vue
@@ -6,16 +6,13 @@
         <h3>Kontaktiere uns per Mail:</h3>
         <a href="mailto: Beispielmail@hs-furtwangen.de"> E-Mail </a>
         <h3>oder fülle unser Kontaktformular aus:</h3>
-        <form id="kontaktformular" name="kontaktformular" action="">
-            <label for="absender">Deine E-Mail-Adresse:</label>
-            <input type="text" id="absender" name="absender" />
-            <div>
-            </div>
-            <label for="nachricht">Deine Nachricht:</label>
-            <textarea id="nachricht" name="nachricht" cols="20" rows="5"></textarea>
-            <div>
-            </div>
+        <form id="Kontaktformular" name="Kontaktformular" action="">
+            <label for="Absender">Deine E-Mail-Adresse:</label>
+            <input type="text" id="Absender" name="Absender" />
+            <label for="Nachricht">Deine Nachricht:</label>
+            <textarea id="Nachricht" name="Nachricht" cols="20" rows="5"></textarea>
             <input type="submit" value="Abschicken" />
+        </form>
         </body>
     </div>
 </template>
