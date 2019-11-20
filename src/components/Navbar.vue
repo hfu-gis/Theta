141d44fa9317ff3f8dff2d24675591ca87c5f07c
leonschafer <leon.schaefer@hs-furtwangen.de>
2019-11-20 15:08:26 +0100

    Navbar hinzugefügt

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span>Ninja</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey"
+                   <span>Sign Out</span>
+            <v-icon
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
