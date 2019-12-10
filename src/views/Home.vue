df0c6b9fdb53c5c7b405a636363d23548e8547de
Svenjaschaefer <svenja.schaefer@hs-furtwangen>
2019-12-10 18:13:30 +0100

    Bearbeitung Yourwordotd-Seite Random word Generator

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="body-1">
-        <h2><a href="Login.vue">Login</a></h2>
-        <h2><a href="Registrierung.vue">Registrierung</a></h2>
+        <h2><a href="Login.vue">Login</a></h2><br>
+        <h2><a href="Registrierung.vue">Registrierung</a></h2></br>
+        <br>
+
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <h3><a href="Kontakt.vue">Kontakt</a></h3>
-        <h3><a href="Datenschutzbestimmungen.vue"> Datenschutzbestimmungen</a></h3>
+        <h3><a href="Nutzungsbedingungen.vue">Nutzungsbedingungen</a></h3>
+        <h3><a href="Impressum.vue">Impressum</a></h3>
+
     </div>
 </template>
 
