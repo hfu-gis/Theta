44b19ab4c76f66ce4aa1d5fa93fdc323cd4730f5
leonschafer <leon.schaefer@hs-furtwangen.de>
2019-12-11 10:52:57 +0100

    Registration von Alfred

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,89 +1,110 @@
 <template>
-    <div class="body-1">
-            <h1><strong> Registration </strong></h1>
+    <v-form ref="form" v-model="valid">
+        <v-row v-if="success" align="center" justify="center">
+            <v-card elevation="10">
+                <v-img src="http://picsum.photos/200"
+                       class="white--text align-end"
+                       gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                >
+                    <v-card-title v-text>Registration<br/>successful</v-card-title>
+                </v-img>
+            </v-card>
+        </v-row>
+        <v-row v-else align="top" justify="center">
+            <v-col cols="12" md="6">
+                <v-text-field label="Nickname" v-model="userData.nickName" required maxlength="25"
+                              :disabled="this.userData.isAlreadyRegistered"
+                              :rules="nameRules"/>
+                <v-text-field label="Password" v-model="userData.password" required  maxlength="25"
+                              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                              :type="showPassword ? 'text' : 'password'"
+                              @click:append="showPassword = !showPassword"
+                              :rules="nameRules"
+                />
+                <v-text-field label="Name" v-model="userData.name" required  maxlength="25"
+                              :rules="nameRules"/>
+                <v-text-field label="Family name" v-model="userData.familyName" required  maxlength="25"
+                              :rules="nameRules"/>
+            </v-col>
 
-            <p> Bitte fülle folgende Informationen aus, um dich zu registrieren:</p>
-        <v-form
-                ref="form"
-                v-model="valid">
-        <v-text-field
-                v-model="username"
-                :rules="usernameRules"
-                label="User Name"
-                required
-        ></v-text-field>
+            <v-col cols="12" sm="6" md="6">
+                <v-text-field label="Street" v-model="userData.streetName" required
+                              :rules="nameRules"/>
+                <v-text-field label="No" v-model="userData.streetNr" required
+                              :rules="nameRules"/>
+                <v-text-field label="ZIP" v-model="userData.zipCode" required
+                              :rules="nameRules"/>
+                <v-text-field label="City" v-model="userData.city" required
+                              :rules="nameRules"/>
+                <v-select label="Country" :items="countries" v-model="userData.country" required clearable
+                          :rules="selectionRules"
+                />
+            </v-col>
+            <v-btn text outlined shaped
+                   @click="validate">Register
+            </v-btn>
 
-        <v-text-field
-                v-model="email"
-                :rules="emailRules"
-                label="HFU E-mail"
-                required
-        ></v-text-field>
-
-        <v-text-field
-                v-model="name"
-                :counter="10"
-                :rules="nameRules"
-                label="Name"
-                required
-        ></v-text-field>
-
-        <v-text-field
-            v-model="nachname"
-            :counter="10"
-            :rueles="NachnameRules"
-            label="Nachname"
-            required
-            ></v-text-field>
-
-
-        <v-select
-                v-model="select"
-                :items="items"
-                :rules="[v => !!v || 'Item is required']"
-                label="Studiengang"
-                required
-        ></v-select>
-
-        <v-checkbox
-                v-model="checkbox"
-                :rules="[v => !!v || 'You must agree to continue!']"
-                label="Ich stimme hiermit den Nutzungsbedingungen zu"
-                required
-        ></v-checkbox>
-
-        <v-btn
-                y:disabled="!valid"
-                color="success"
-                class="mr-4"
-                @click="validation"
-        >
-            Registrieren
-        </v-btn>
-
-
-            <button type="button" onclick="alert('Du bist nur noch wenige Schritte entfernt! In wenigen Minuten bekommst du eine E-Mail mit einem Bestätigungslink von uns zugesendet!')">Registrieren</button>
-        </v-form>
-    </div>
+        </v-row>
+    </v-form>
 </template>
 
 <script>
+    import db from '../db'
+
     export default {
-        name: "Registrierung",
+        props: {},
+        data: () => ({
+            success: false,
+            valid: true,
+            showPassword: false,
+            userData: {
+                nickName: '',
+                password: '',
+                name: '',
+                familyName: '',
+                streetName: '',
+                streetNr: undefined,
+                zipCode: '',
+                city: '',
+                country: '',
+                isAlreadyRegistered: false
+            },
+            countries: [],
+
+            nameRules: [
+                value => (!!value && value.trim().length > 0) || 'Field is required',
+                value => (value && value.length <= 25) || 'Field must be less than 25 characters',
+            ],
+            selectionRules: [
+                value => !!value || 'Field is required'
+            ]
+        }),
+
         methods: {
-            validation() {
+            validate() {
                 if (this.$refs.form.validate()) {
-                    alert("sucsess")
+                    console.debug('Validation success')
+                    this.register()
                 }
+            },
+
+            register()  {
+                this.userData.isAlreadyRegistered = true
+                let docRef = db.collection('User').doc( this.userData.nickName)
+                docRef.set(this.userData)
+                    .catch(error => console.debug('Error', error))
+                    .then(() => this.success = true)
+                // docRef.update(userData)
             }
 
         },
-        data: () => ({
-            valid: true
-        })
+        created() {
+            let docRef = db.collection('LegalCountries')
+            docRef.get().then(docs => {
+                docs.forEach(doc => this.countries.push(doc.data().name))
+            })
+            docRef = db.collection('User').doc( 'Artingo')
+            docRef.get().then(doc => this.userData = doc.data())
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
