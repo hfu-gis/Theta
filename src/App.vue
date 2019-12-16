9821db7edb5662193a346f27c799658fc94b5b14
erpunkt <rebecca.rachel.raeschke@hs-furtwangen.de>
2019-12-16 19:25:58 +0100

    Navbar fix

--- src/App.vue
+++ src/App.vue
@@ -2,52 +2,62 @@
   <v-app id="inspire">
     <v-navigation-drawer
             v-model="drawer"
-            app dark
-            right
+            app
+            clipped-left
+            color="grey lighten-4"
     >
       <v-list dense>
         <v-list-item link :to="{name:'Home'}">
           <v-list-item-action>
-            <v-icon>mdi-spa</v-icon>
+            <v-icon>mdi-home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title>Home</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item link :to="{name:'Wordotd'}">
+        <v-list-item link :to="{name:'Login'}">
           <v-list-item-action>
-            <v-icon>mdi-cart</v-icon>
+            <v-icon>mdi-login</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Word of the Day</v-list-item-title>
+            <v-list-item-title>Login</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link :to="{name:'Yourwordotd'}">
+          <v-list-item-action>
+            <v-icon>mdi-file-word</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Your Word of the Day</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link :to="{name:'Registrierung'}">
           <v-list-item-action>
-            <v-icon>mdi-settings</v-icon>
+            <v-icon>mdi-account-plus</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Registrierung</v-list-item-title>
+            <v-list-item-title>Registrieren</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link :to="{name:'Kontakt'}">
+          <v-list-item-action>
+            <v-icon>mdi-account-question</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Kontakt</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app right dark
-               src="http://picsum.photos/1920/100?blur">
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"  />
+      <v-spacer />
       <v-toolbar-title>D(M)ICTIONARY</v-toolbar-title>
       <v-spacer />
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-      <v-btn icon :to="{name:'Order'}">
-        <v-icon>mdi-cart</v-icon>
-      </v-btn>
-
       <v-btn v-if="!isLoggedIn" icon :to="{name:'Login'}">
         <v-icon>mdi-login</v-icon>
       </v-btn>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"  />
     </v-app-bar>
 
     <v-content light>
