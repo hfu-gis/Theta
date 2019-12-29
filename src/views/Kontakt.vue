a8d887872abdf199887e4381ff6dfb58b0ff1ce8
Svenjaschaefer <svenja.schaefer@hs-furtwangen>
2019-12-29 17:12:33 +0100

    Bearbeitung Kontakt Button eingefügt

--- src/views/Kontakt.vue
+++ src/views/Kontakt.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="body-1">
         <h1>Kontakt</h1>
+        <div>
         <h2>Ansprechpartner: Team Theta</h2>
         <h3>Kontaktiere uns per Mail:</h3>
-        <a href="mailto: Beispielmail@hs-furtwangen.de"> E-Mail </a>
+        </div>
+        <a href="mailto: dm.dictionary-support.de">
+                <v-btn text outlined shaped
+                       oneclick="e-mail">E-Mail
+                </v-btn>
+        </a>
         <h3>oder fülle unser Kontaktformular aus:</h3>
         <form id="Kontaktformular" name="Kontaktformular" action="">
             <label for="Absender">Deine E-Mail-Adresse:</label>
+
             <input type="text" id="Absender" name="Absender" />
             <label for="Nachricht">Deine Nachricht:</label>
             <textarea id="Nachricht" name="Nachricht" cols="20" rows="5"></textarea>
--- src/views/Yourwordotd.vue
+++ src/views/Yourwordotd.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="body-1">
         <h1> Your Word of the Day </h1>
-        <FORM NAME="WordForm">
+        <FORM name="WordForm">
             <input type="Text" name="WordBox" id="wordbox">
             <v-btn text outlined shaped
+                   justify="center"
                    oneclick="generate">generieren
             </v-btn>
         </FORM>
@@ -16,17 +17,6 @@
 </script>
 <style scoped>
     /* CSS für diese Seite hier einfügen */
-    body {
-        font-family: Verdana, Geneva, sans-serif;
-        font-size: 1.2em;
-        margin: 0 auto;
-        max-width: 960px;
-        padding: 40px 10px;
-        line-height: 1.65em;
-        background: #B20940;
-        color: #fff;
-        font-weight: 300;
-    }
 
     h1 {
         text-align: center;
@@ -35,6 +25,7 @@
 
     #wordbox {
         /*opacity: 0;*/
+        text-allign: center;
         margin: 30px auto 0;
         display: block;
         width: 300px;
