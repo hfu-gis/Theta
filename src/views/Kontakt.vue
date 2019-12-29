3bb2a90b54355ccce0d452cce4c4d49268a80b2a
Svenjaschaefer <svenja.schaefer@hs-furtwangen>
2019-12-29 17:21:51 +0100

    Bearbeitung Kontakt Layout bearbeitet

--- src/views/Kontakt.vue
+++ src/views/Kontakt.vue
@@ -3,6 +3,7 @@
         <h1>Kontakt</h1>
         <div>
         <h2>Ansprechpartner: Team Theta</h2>
+
         <h3>Kontaktiere uns per Mail:</h3>
         </div>
         <a href="mailto: dm.dictionary-support.de">
@@ -10,16 +11,21 @@
                        oneclick="e-mail">E-Mail
                 </v-btn>
         </a>
+
         <h3>oder fülle unser Kontaktformular aus:</h3>
         <form id="Kontaktformular" name="Kontaktformular" action="">
-            <label for="Absender">Deine E-Mail-Adresse:</label>
+            <label for="absender">Deine E-Mail-Adresse:</label>
+            <input type="Text" name="WordBox" id="wordbox">
 
-            <input type="text" id="Absender" name="Absender" />
-            <label for="Nachricht">Deine Nachricht:</label>
-            <textarea id="Nachricht" name="Nachricht" cols="20" rows="5"></textarea>
-            <input type="submit" value="Abschicken" />
+            <input type="text" id="Absender" name="Absender"/>
+            <label for="nachricht">Deine Nachricht:</label>
+            <input type="Text" name="WordBox" id="wordbox">
+            <v-btn text outlined shaped
+                   oneclick="absenden">absenden
+            </v-btn>
         </form>
     </div>
+
 </template>
 
 <script>
@@ -29,5 +35,19 @@
 </script>
 
 <style scoped>
+    #wordbox {
+        /*opacity: 0;*/
+        text-allign: center;
+        margin: 30px auto 0;
+        display: block;
+        width: 300px;
+        height: 40px;
+        font-size: 30px;
+        text-align: center;
+        background: #fff;
+        border-radius: 6px;
+        color: #000000;
+        transition: 1s linear;
+    }
 
 </style>
